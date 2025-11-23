23ab30b97aba92f69e8c45e08e6d67c9feb4723f
Gleb Colei <gleb.koley@gmail.com>
2025-11-24 00:26:50 +0200

    fix mobile header(2)

--- src/lib/components/Layout/Header.jsx
+++ src/lib/components/Layout/Header.jsx
@@ -15,7 +15,7 @@ const Header = ({ links = [] }) => {
    return (
       <>
          <header
-            className={`tablet:pb-2 desktop:pb-0 tablet:pt-5 tablet:justify-self-center tablet:bg-transparent fixed z-50 w-full ${isMainPage ? 'bg-[#00000063]' : 'bg-transparent'} px-4 pt-3 pb-1.5`}
+            className={`tablet:pb-2 desktop:pb-0 tablet:pt-5 tablet:justify-self-center tablet:bg-transparent tablet:backdrop-blur-[0px] tablet:border-0 fixed z-50 w-full ${isMainPage ? 'bg-mainBlack/70 border-b border-white/20 backdrop-blur-md' : 'bg-transparent'} px-4 pt-3 pb-1.5`}
          >
             <nav
                className={`tablet:flex mx-auto hidden w-max items-center justify-center rounded-[100px] bg-[#211f2359] px-6 py-3 backdrop-blur-[20px]`}
