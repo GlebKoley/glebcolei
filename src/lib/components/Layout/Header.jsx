2e3595cf5550b6b66fd083573791eb4f401bfd35
Gleb Colei <gleb.koley@gmail.com>
2025-11-24 21:19:30 +0200

    fix header and add blur

--- src/lib/components/Layout/Header.jsx
+++ src/lib/components/Layout/Header.jsx
@@ -15,13 +15,12 @@ const Header = ({ links = [] }) => {
    return (
       <>
          <header
-            className={`tablet:pb-2 desktop:pb-0 tablet:pt-5 tablet:justify-self-center tablet:bg-transparent tablet:backdrop-blur-[0px] tablet:border-0 fixed z-50 w-full ${isMainPage ? 'bg-mainBlack/70 border-b border-white/20 backdrop-blur-md' : 'bg-transparent'} px-4 pt-3 pb-1.5`}
+            className={`tablet:pb-2 desktop:pb-0 tablet:pt-5 tablet:justify-self-center tablet:bg-transparent tablet:border-0 fixed z-50 w-full ${isMainPage ? 'bg-mainBlack/70 border-b border-white/10' : ''} px-4 pt-3 pb-1.5`}
          >
-            <nav
-               className={`tablet:flex mx-auto hidden w-max items-center justify-center rounded-[100px] bg-[#211f2359] px-6 py-3 backdrop-blur-[20px]`}
-            >
+            <nav className="tablet:flex bg-[rgba(33, 31, 35, 0.35)] mx-auto hidden w-max items-center justify-center rounded-[100px] border border-white/10 px-6 pt-2.5 pb-3.5 backdrop-blur-[20px]">
                {renderHeaderLinks({ links, pathname })}
             </nav>
+
             <BurgerMenu data={{ links, pathname }} />
          </header>
          <div className="tablet:h-[90px]" />
