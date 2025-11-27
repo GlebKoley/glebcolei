51b964a032f7463c5c43ab9cd7aaf52b3ce2d83f
Gleb Colei <gleb.koley@gmail.com>
2025-11-27 14:35:38 +0200

    fix header

--- src/lib/components/Layout/Header.jsx
+++ src/lib/components/Layout/Header.jsx
@@ -15,7 +15,7 @@ const Header = ({ links = [] }) => {
    return (
       <>
          <header
-            className={`tablet:pb-2 desktop:pb-0 tablet:pt-5 tablet:justify-self-center tablet:bg-transparent tablet:border-0 fixed z-50 w-full ${isMainPage ? 'bg-mainBlack/70 border-b border-white/10' : ''} px-4 pt-3 pb-1.5`}
+            className={`tablet:w-max tablet:pb-2 desktop:pb-0 tablet:pt-5 tablet:justify-self-center tablet:bg-transparent tablet:border-0 fixed z-50 w-full ${isMainPage ? 'bg-mainBlack/70 border-b border-white/10' : ''} px-4 pt-3 pb-1.5`}
          >
             <nav className="tablet:flex bg-[rgba(33, 31, 35, 0.35)] mx-auto hidden w-max items-center justify-center rounded-[100px] border border-white/10 px-6 pt-2.5 pb-3.5 backdrop-blur-[20px]">
                {renderHeaderLinks({ links, pathname })}
